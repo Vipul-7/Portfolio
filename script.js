ef8e5e5bb66c9bffb7581cbd61ec34b2c625c012
Vipul Chaudhary <vipschaudhary044@gmail.com>
2023-06-11 00:14:05 +0530

    photos and css edited

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// // scroll behavior
-// window.addEventListener("scroll", function () {
-//   const navBar = this.document.querySelector(".mainNav");
-//   const threshold = 100;
+// scroll behavior
+window.addEventListener("scroll", function () {
+  const navBar = this.document.querySelector(".mainNav");
+  const threshold = 100;
 
-//   if (window.pageYOffset > threshold) {
-//     navBar.classList.add("scrolled");
-//   } else {
-//     navBar.classList.remove("scrolled");
-//   }
-// });
+  if (window.pageYOffset > threshold) {
+    navBar.classList.add("scrolled");
+  } else {
+    navBar.classList.remove("scrolled");
+  }
+});
 
 // mobile navigation
 const btnNav = document.querySelector(".btn-mobile-nav");
